7275aad89a54d0bc0d489f9621c84c322be0d270
MarineColtel <marine_coltel@hotmail.fr>
2025-03-20 11:28:04 +0100

    initiliazing project

---
+++ src/main/java/com/example/sutombis/SutombisApplication.java
@@ -0,0 +1,13 @@
+package com.example.sutombis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SutombisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SutombisApplication.class, args);
+	}
+
+}
