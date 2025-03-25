41a44560893254e345ac91d0315fb24f9e9997d2
MarineColtel <marine_coltel@hotmail.fr>
2025-03-25 09:04:25 +0100

    connect to catfactapi

--- src/main/java/com/example/sutombis/controller/ConnectApiController.java
+++ src/main/java/com/example/sutombis/controller/ConnectApiController.java
@@ -5,10 +5,27 @@ import org.springframework.web.client.RestTemplate;
 
 import com.example.sutombis.model.CatFact;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 @RestController
 public class ConnectApiController {
-     String url = "https://catfact.ninja/fact";
-    RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/catfact")
+    public String getRandomCatFact(Model model) {
+        String url = "https://catfact.ninja";
+        RestTemplate restTemplate = new RestTemplate();
+        
+        CatFact response = restTemplate.getForObject(url + "/fact", CatFact.class);
+        
+        return response.getFact();
+    }
     
-    CatFact response = restTemplate.getForObject(url, CatFact.class);
+    // @PostMapping("/catfact")
+    // public String postRandomCatFact(@ModelAttribute CatFact catFact, Model model) {
+    //     model.addAttribute("catfact", catFact);
+    //     return "CatFact";
+    // }
+     
 }
--- src/main/java/com/example/sutombis/model/CatFact.java
+++ src/main/java/com/example/sutombis/model/CatFact.java
@@ -1,5 +1,33 @@
 package com.example.sutombis.model;
 
 public class CatFact {
-    String catFact;
+    private long id;
+    private String fact;
+    private Number length;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getFact() {
+    return fact;
+  }
+
+  public void setFact(String fact) {
+    this.fact = fact;
+  }
+
+  public Number getLength() {
+    return length;
+  }
+
+  public void setLength(Number length) {
+    this.length = length;
+  }
+
+
 }
