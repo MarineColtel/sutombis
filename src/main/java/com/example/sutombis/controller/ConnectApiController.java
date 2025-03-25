f1266406ccfe31d2fb239ffb4b66f45f1d0916b7
MarineColtel <marine_coltel@hotmail.fr>
2025-03-25 11:22:39 +0100

    create sutom model and starting controller

--- src/main/java/com/example/sutombis/controller/ConnectApiController.java
+++ src/main/java/com/example/sutombis/controller/ConnectApiController.java
@@ -4,14 +4,30 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.sutombis.model.CatFact;
+import com.example.sutombis.model.Sutom;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 public class ConnectApiController {
 
+    // Connecting to Catfact ninja API
     @GetMapping("/catfact")
     public String getRandomCatFact(Model model) {
         String url = "https://catfact.ninja";
@@ -27,5 +43,37 @@ public class ConnectApiController {
     //     model.addAttribute("catfact", catFact);
     //     return "CatFact";
     // }
-     
+
+
+    // Connecting to WordsAPI api
+
+    @GetMapping("/get_sutom_word")
+    public String getRandomWord(Model model) {
+        String url = "https://trouve-mot.fr/api";
+        RestTemplate restTemplate = new RestTemplate();
+        
+        Sutom response = restTemplate.getForObject(url + "/daily", Sutom.class);
+        
+        return response.getName();
+    }
+
+
+
+    //  @GetMapping("/get_sutom_word")
+    // public String getRandomWord(Model model) throws IOException {
+
+    //     String url = "https://wordsapiv1.p.rapidapi.com/words/lovely/synonyms";
+
+    //     HttpHeaders headers = new HttpHeaders();
+    //     headers.add("x-rapidapi-host", "wordsapiv1.p.rapidapi.com");
+    //     headers.add("x-api-key", "66415cdb44msh4c25cde97367049p176f34jsnd9aca8060cfd");
+        
+    //     HttpEntity<Object> entity = new HttpEntity<Object>(headers);
+    //     RestTemplate restTemplate = new RestTemplate();
+    //     ResponseEntity<Sutom> response = restTemplate.exchange(url, HttpMethod.GET, entity, Sutom.class);
+        
+    //     return response.toString();
+    // }
+    
+    
 }
---
+++ src/main/java/com/example/sutombis/model/Sutom.java
@@ -0,0 +1,33 @@
+package com.example.sutombis.model;
+
+public class Sutom {
+
+    private long id;
+    private String name;
+    private String categorie;
+
+    public long getId() {
+        return id;
+      }
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+}
